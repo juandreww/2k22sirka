72980c9af64f28fd97d223e53e565e0ccf5ac100
juandreww <juandreww@outlook.com>
2022-08-23 21:35:54 +0700

    views

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
-			data := structs.Response {
+			data := views.Response {
 				Code: http.StatusOK,
 				Body: "pong",
 			}
---
+++ model/ping.go
@@ -0,0 +1,24 @@
+package controller
+
+import (
+	"net/http"
+	"encoding/json"
+	"2k22sirka/views"
+	"fmt"
+)
+
+func ping() http.HandlerFunc {
+	fmt.Println("ping")
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			fmt.Println("Request received")
+			data := views.Response {
+				Code : http.StatusOK,
+				Body : "pong",
+			}
+			
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(data)
+		}
+	}
+}
